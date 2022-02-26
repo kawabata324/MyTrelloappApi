8c3c478e540223bafb856de48be823f901c93104
kawabata <s1931115@s.konan-u.ac.jp>
2022-02-26 19:24:05 +0900

    不要な箇所を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
-        # include ApiResponseHandler
 
         before_action :configure_permitted_parameters, if: :devise_controller?
 
